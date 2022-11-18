6a1cdcaca736942ff6622c5c96f123af3866b9c6
Cem Mehmet Onal <mehmetcemonal@gmail.com>
2022-11-18 18:39:44 -0500

    :sparkles:

---
+++ Loops.js
@@ -0,0 +1,8 @@
+//Write a for loop that logs 0-15
+//for (let i = 0; i <= 15; i++) {console.log(i)}
+
+//Write a for loop that counts from 15-0
+//for (let i = 15; i >= 0; i--) {console.log(i)}
+
+//Make variable laps = 0; write a for loop that logs (“that’s another lap!”) and adds 1 to the laps variable. AFTER the loop, log laps.
+//laps = 0 ; for(let i = 0; i < 15; i++) {laps++;console.log("that’s another lap!")}; console.log(laps);
